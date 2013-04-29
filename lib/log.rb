94a9443f378b80750d7c07f7a12fdf64fef4e111
Donald Mennerich <donaldmennerich@nypl.org>
2013-04-29 10:13:37 -0400

    adding event log

--- lib/log.rb
+++ lib/log.rb
@@ -1,7 +1,7 @@
 module EriLog
   def log_search(user, qType, q)
     string = Time.now.to_i.to_s + "\t QUERY\t" + user + "\t" + qType + "\t" + q + "\n"
-    File.open('log/queries.log','a') do|f|
+    File.open('log/events.log','a') do|f|
       f.write(string)
       f.close
     end
@@ -9,7 +9,7 @@ module EriLog
   
   def log_file(user, file)
     string = Time.now.to_i.to_s + "\t FILE\t" + user + "\t" + file + "\n"
-    File.open('log/queries.log','a') do|f|
+    File.open('log/events.log','a') do|f|
       f.write(string)
       f.close
     end
